70d3ae5437c0bd12d03c78bc45d9d968033e8bd6
Yuliya1913 <Lera-855@email.ua>
2023-03-10 20:35:40 +0200

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,32 +8,38 @@ const formData = {};
 
 const formEl = document.querySelector('.feedback-form');
 // console.log(formEl);
-const inputEl = formEl.firstElementChild;
-// console.log(inputEl);
-const textareaEl = inputEl.nextElementSibling;
-// console.log(textareaEl);
 
-formEl.addEventListener('input', onFormInput);
+const inputEl = document.querySelector('[name="email"]');
+
+// const inputEl = formEl.firstElementChild.firstElementChild;
+console.log(inputEl);
+const textareaEl = document.querySelector('[name="message"]');
+console.log(textareaEl);
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
 populateForm();      
   
 function onFormInput(event) {
+    
 // Создаем объект: ключ - значение
     formData[event.target.name] = event.target.value;
+
     // console.log(event.target.name);
     // console.log(event.target.value);
 
 // Заносим объект данных в локальное хранилище
-    const formDataSet = localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
     
 }
 
 function populateForm() {
-    // получаем значение из localStorage
- const formDataGet = JSON.parse(localStorage.getItem(FORM_KEY));
-console.log(formDataGet);
+    // получаем значение из localStorage и т.к.это объект - применим parse 
+    const formDataGet = JSON.parse(localStorage.getItem(FORM_KEY));
+    console.log(formDataGet);
 
+    // если в formDataGet есть данные, то внести их в инпут и текстерию
  if (formDataGet) {
      inputEl.value = formDataGet.email;
      textareaEl.value = formDataGet.message;
@@ -53,7 +59,7 @@ function onFormSubmit(event) {
     event.currentTarget.reset();
     // очищаем localStorage
     localStorage.removeItem(FORM_KEY);
-
+    
     console.log(formData);
 
 }
