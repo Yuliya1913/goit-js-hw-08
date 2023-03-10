78d2aaf45f139e2ba5ff8c09485a96395a11061a
Yuliya1913 <Lera-855@email.ua>
2023-03-10 18:08:28 +0200

    add 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,25 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+
+console.log(Player);
+
+
+const TIME_KEY = "videoplayer-current-time"
+
+    const iframe = document.querySelector('iframe');
+const player = new Player(iframe); 
+    
+// const timeEl = localStorage.setItem(TIME_KEY, event.second);
+console.log(timeEl)
+
+player.on('play', function (event) {
+    const timeEl = localStorage.setItem(TIME_KEY, event.seconds);
+        console.log(timeEl)
+        console.log('played the video!')
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title)
+    });
+
 
 // console.log(Player);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+import throttle from 'lodash.throttle';
+console.log(throttle);
+const FORM_KEY = "feedback-form-state";
+// Создаем объект
+const formData = {};
+
+// находим ссылки на form, input, extarea
+
+const formEl = document.querySelector('.feedback-form');
+// console.log(formEl);
+const inputEl = formEl.firstElementChild;
+// console.log(inputEl);
+const textareaEl = inputEl.nextElementSibling;
+// console.log(textareaEl);
+
+formEl.addEventListener('input', onFormInput);
+formEl.addEventListener('submit', onFormSubmit);
+
+populateForm();      
+  
+function onFormInput(event) {
+// Создаем объект: ключ - значение
+    formData[event.target.name] = event.target.value;
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+
+// Заносим объект данных в локальное хранилище
+    const formDataSet = localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+    
+}
+
+function populateForm() {
+    // получаем значение из localStorage
+ const formDataGet = JSON.parse(localStorage.getItem(FORM_KEY));
+console.log(formDataGet);
+
+ if (formDataGet) {
+     inputEl.value = formDataGet.email;
+     textareaEl.value = formDataGet.message;
+
+     console.log(inputEl.value);
+     console.log(textareaEl.value);  
+    }
+}
+
+// при отправке формы
+function onFormSubmit(event) {
+    console.log(event);
+
+    event.preventDefault();
+
+    // очищаем форму
+    event.currentTarget.reset();
+    // очищаем localStorage
+    localStorage.removeItem(FORM_KEY);
+
+    console.log(formData);
+
+}
+
+
 
